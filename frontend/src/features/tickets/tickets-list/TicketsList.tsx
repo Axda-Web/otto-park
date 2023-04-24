1522315a9e1a58f8ff39fce303998be979265ce4
Alyx DARENNE <axda.web@gmail.com>
2023-04-24 11:53:29 +0200

    Home page: Add responsivness + Refactoring

--- frontend/src/features/tickets/tickets-list/TicketsList.tsx
+++ frontend/src/features/tickets/tickets-list/TicketsList.tsx
@@ -19,6 +19,10 @@ const TicketsList = () => {
 
 	const tickets = data?.map((ticket) => <Ticket key={ticket.id} {...ticket} />);
 
-	return <div className="grid grid-cols-6 gap-10 mt-20">{tickets}</div>;
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-10 mt-20">
+			{tickets}
+		</div>
+	);
 };
 export default TicketsList;
--- frontend/src/routes/root/Root.tsx
+++ frontend/src/routes/root/Root.tsx
@@ -4,8 +4,8 @@ import TicketsList from '../../features/tickets/tickets-list';
 const Root = () => {
 	return (
 		<main>
-			<section className="flex flex-col align-center h-[calc(100vh-120px)] pt-80 gap-y-20">
-				<h2 className="capitalize text-center text-9xl font-bold">
+			<section className="flex flex-col align-center h-[calc(100vh-120px)] pt-80 px-5 sm:px-20 gap-y-20">
+				<h2 className="capitalize text-center text-5xl md:text-7xl lg:text-9xl font-bold">
 					Parking made easy
 				</h2>
 				<HashLink
@@ -16,8 +16,11 @@ const Root = () => {
 					Book a space
 				</HashLink>
 			</section>
-			<section id="parking-spot-picking" className="h-screen bg-gray-100 p-20 relative">
-				<h2 className="text-center text-5xl font-bold">
+			<section
+				id="parking-spot-picking"
+				className="min-h-screen bg-gray-100 px-6 md:px-8 lg:px-10 xl:px-20 py-14 sm:py-20 relative"
+			>
+				<h2 className="text-center text-3xl md:text-5xl font-bold">
 					Just pick a spot and you're good to go!
 				</h2>
 				<TicketsList />
