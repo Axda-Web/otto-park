e5229bcefaee1037845aedc204b569579765fbfd
Alyx DARENNE <axda.web@gmail.com>
2023-04-24 09:05:34 +0200

    Home page: Update home page + Add error page

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
 					<h1 className="font-black text-4xl">Otto</h1>
 				</Link>
 				<nav className="text-xl flex gap-x-14 uppercase underline">
-          {/* Alyx: Add conditional navlink rendering for other pages */}
+					{/* Alyx: Add conditional navlink rendering for other pages */}
 					<Link to="/login">Log in</Link>
 				</nav>
 			</div>
--- frontend/src/routes/root/Root.tsx
+++ frontend/src/routes/root/Root.tsx
@@ -1,5 +1,29 @@
+import { HashLink } from 'react-router-hash-link';
+import TicketsList from '../../features/tickets/tickets-list';
+
 const Root = () => {
-	return <div>Root</div>;
+	return (
+		<main>
+			<section className="flex flex-col align-center h-[calc(100vh-120px)] pt-80 gap-y-20">
+				<h2 className="capitalize text-center text-9xl font-bold">
+					Parking made easy
+				</h2>
+				<HashLink
+					smooth
+					to="#parking-spot-picking"
+					className="text-xl bg-black text-white block w-fit mx-auto	px-10 py-4 rounded uppercase"
+				>
+					Book a space
+				</HashLink>
+			</section>
+			<section id="parking-spot-picking" className="h-screen bg-gray-100 p-20 relative">
+				<h2 className="text-center text-5xl font-bold">
+					Just pick a spot and you're good to go!
+				</h2>
+				<TicketsList />
+			</section>
+		</main>
+	);
 };
 
 export default Root;
